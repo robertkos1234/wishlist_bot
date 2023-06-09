b089bfff86dc271148497cb7f0a2f18b1243ec13
robertkos1234 <robert.kos1234@gmail.com>
2023-06-09 15:23:27 +0300

    v 0.0.1

---
+++ main.py
@@ -0,0 +1,10 @@
+import telebot
+
+token = "6163537042:AAGDTzM6pSbcXSQToZZTKzXLjsm7wn89CXc"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,'кукусик')
+
+bot.polling(none_stop=True)
